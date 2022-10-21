ef2014c1539804e5eba065929b827bb27145432c
b4nned21 <115525959+b4nned21@users.noreply.github.com>
2022-10-21 19:54:12 +0300

    Conversion seconds

--- Lesson_1/task3.py
+++ Lesson_1/task3.py
@@ -1,16 +1,11 @@
-finish = ("")
-while finish != "exit":
-    sec_user = int(input("Enter the number of seconds: "))
+sec_user = int(input("Enter the number of seconds: "))
+sec = sec_user % (24 * 3600)
+min = sec_user // 60
+sec %= 60
+hours = min // 60
+min %= 60
+days = hours // 24
+hours %= 24
+print(f"Days: {days};\nHours: {hours};\nMinutes: {min};\nSeconds: {sec}.")
 
-    sec = sec_user % (24 * 3600)
-    min = sec_user // 60
-    sec %= 60
-    hours = min // 60
-    min %= 60
-    days = hours // 24
-    hours %= 24
-    print(f"Days: {days};\nHours: {hours};\nMinutes: {min};\nSeconds: {sec}.")
-
-    finish = input("Type 'exit' to finish: ")
-
-
+input("Press Enter to close the program")
