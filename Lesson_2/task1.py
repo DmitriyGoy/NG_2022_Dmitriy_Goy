329da46d845f9ceddc85d32d2ece75528be0dd9e
DmitriyGoy <d.g21052005@gmail.com>
2022-11-14 17:48:48 +0200

    The 'string' variable is injected into the function

--- Lesson_2/task1.py
+++ Lesson_2/task1.py
@@ -1,17 +1,18 @@
-def number_of_letters(element):
+def number_of_letters(element, string):
     if element != " ":
         amount = string.count(element)
         print(f"{element} - {amount}; ", end = "")
+        return 0
 #function to display letters and their number in a string.
 
 string = input("Enter the string: ").lower()
 
 print("All characters in a string: ", end = "")
 for element in set(string):
-    number_of_letters(element)
+    number_of_letters(element, string)
 
 print("\n==================================================================================")
 
 print("All sorting characters in a string: ", end = "")
 for element in sorted(set(string)):
-    number_of_letters(element)
+    number_of_letters(element, string)
