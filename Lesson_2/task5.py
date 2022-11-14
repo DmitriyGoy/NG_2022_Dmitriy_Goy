7ad74971a44107a5da2e6e5f954c83bf77318aa3
DmitriyGoy <d.g21052005@gmail.com>
2022-11-14 17:44:12 +0200

    Replaced the loop for removing the maximum and minimum of the list with a function

--- Lesson_2/task5.py
+++ Lesson_2/task5.py
@@ -2,21 +2,13 @@ list = input("Enter list items: ").split(",")
 print("===========================================================================================")
 
 list_int = []
-for elem in range(len(list)):
-    list_int.insert(elem, int(list[elem]))
+for num in range(len(list)):
+    list_int.insert(num, int(list[num]))
 
-maximum = max(list_int)
-minimum = min(list_int)
+maximum = list_int.pop(list_int.index(max(list_int)))
+minimum = list_int.pop(list_int.index(min(list_int)))
 
 print(f"The largest element from the list: {maximum};")
 print(f"The smallest element from the list: {minimum};")
-
-for number in list_int:
-    if number == maximum:
-        list_int.remove(number)
-        
-for number in list_int:
-    if number == minimum:
-        list_int.remove(number)
 
 print(f"\nSum of all other elements: {sum(list_int)}.")
